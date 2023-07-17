a559fe0ab6a2bfe937a31ccca1fd6bc870fed578
TinaBlshvl <pro.web.design.tina@gmail.com>
2023-07-17 13:32:03 +0200

    HW_06

--- controllers/contacts/addNew.js
+++ controllers/contacts/addNew.js
@@ -1,4 +1,4 @@
-const { Contact } = require("../../models/contact");
+const { Contact } = require("../../models/contact/contact");
 
 const addNew = async (req, res) => {
   const { _id: owner } = req.user;
